3a0522c7e518bb3554fb52f0f858fd30145cb5b6
Kairo Lamarca <lamarca.kairo@gmail.com>
2017-10-03 23:51:27 -0300

    Classes de dominio
    classes uf, cep, endereco e telefone e textohelper

---
+++ ProjetoEcommerce.Domain.Tests/ValueObject/EmailTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetoEcommerce.Domain.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEcommerce.Domain.Tests.ValueObject
+{
+    [TestClass]
+    public class EmailTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Email_New_Email_Em_Branco()
+        {
+            var email = new Email("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Email_New_Email_Null()
+        {
+            var email = new Email(null);
+        }
+
+        [TestMethod]
+        public void Email_New_Email_Valido()
+        {
+            var endereco = "lamarca.kairo@gmail.com";
+            var email = new Email(endereco);
+            Assert.AreEqual(endereco, email.Endereco);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Email_New_Email_Invalido()
+        {
+            var email = new Email("dfsdfsdsa");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Email_New_Email_MaxLenght()
+        {
+            var endereco = "lamarca.kairo@gmail.com";
+            while(endereco.Length < Email.EnderecoMaxLenght + 1)
+            {
+                endereco = endereco + "lamarca.kairo@gmail.com";
+            }
+            var email = new Email(endereco);
+        }
+    }
+}
--- ProjetoEcommerce.Domain/ValueObject/Email.cs
+++ ProjetoEcommerce.Domain/ValueObject/Email.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ProjetoEcommerce.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ProjetoEcommerce.Domain.ValueObject
@@ -10,5 +12,28 @@ namespace ProjetoEcommerce.Domain.ValueObject
     {
         public const int EnderecoMaxLenght = 254;
         public string Endereco { get; private set; }
+
+        //construtor do Entity Framework
+        protected Email()
+        {
+
+        }
+
+        public Email(string endereco)
+        {
+            Guard.ForNullOrEmptyDefaultMessage(endereco, "E-mail");
+            Guard.StringLength("E-mail", endereco, EnderecoMaxLenght);
+
+            if (IsValid(endereco) == false)
+                throw new Exception("E-mail inválido");
+
+            Endereco = endereco;
+        }
+
+        public static bool IsValid(string email)
+        {
+            var regexEmail = new Regex(@"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
+            return regexEmail.IsMatch(email);
+        }
     }
 }
